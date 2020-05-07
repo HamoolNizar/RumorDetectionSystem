faaeda4ad0c989635d0b8f13ce8f847b742d6e21
HamoolNizar <hamool.2015343@iit.ac.lk>
2020-05-08 04:47:32 +0530

    fix (eval): evaluation script

--- data/eval/eval.pl
+++ data/eval/eval.pl
@@ -48,7 +48,7 @@ if(@guessLns != @goldLns){
 	die "\n";
 }
 
-my @cats = ("FAVOR", "AGAINST", "NONE", "UNKNOWN");
+my @cats = ("RUMOR", "NON-RUMOR", "UNKNOWN");
 my %catsHash = map{$_ => 1}@cats;
 my %numOfTruePosOfEachCat = ();
 my %numOfGuessOfEachCat = ();
@@ -60,21 +60,21 @@ for (my $i = 0; $i < @guessLns; $i+=1) {
 	my $goldLn = $goldLns[$i];
 	$goldLn =~ s/\r//g;
 
-	if($goldLn eq "ID	Target	Tweet	Stance"){
+	if($goldLn eq "ID	Target	Tweet	Veracity"){
 		next;
 	}
 
 	my @goldArr = split(/\t/, $goldLn);
 	if(@goldArr != 4){
 		print STDERR sprintf("\nError: the following line in the gold-standard file does not have a correct format:\n\n%s\n\n",$goldLn);
-		print STDERR "Correct format: ID<Tab>Target<Tab>Tweet<Tab>Stance\n";
+		print STDERR "Correct format: ID<Tab>Target<Tab>Tweet<Tab>Veracity\n";
 		die "\n";
 	}
 
 	my @guessArr = split(/\t/, $guessLn);
 	if(@guessArr != 4){
 		print STDERR sprintf("\nError: the following line in your prediction file does not have a correct format:\n\n%s\n\n",$guessLn);
-		print STDERR "Correct format: ID<Tab>Target<Tab>Tweet<Tab>Stance";
+		print STDERR "Correct format: ID<Tab>Target<Tab>Tweet<Tab>Veracity";
 		die "\n";
 	}
 
@@ -84,13 +84,13 @@ for (my $i = 0; $i < @guessLns; $i+=1) {
 
 	if(!defined($catsHash{$goldLbl})){
 		print STDERR sprintf("\nError: the stance label \"%s\" in the following line of the gold-standard file is invalid:\n\n%s\n\n",$goldLbl, $goldLn);
-		print STDERR "Correct labels in gold-standard file can be: FAVOR, AGAINST, NONE, or UNKNOWN (case sensitive). \n";
+		print STDERR "Correct labels in gold-standard file can be: RUMOR, NON-RUMOR, or UNKNOWN (case sensitive). \n";
 		die "\n";
 	}
 
 	if(!defined($catsHash{$guessLbl})){
 		print STDERR sprintf("\nError: the stance label \"%s\" in the following line of the prediction file is invalid:\n\n%s\n\n",$guessLbl, $guessLn);
-		print STDERR "Correct labels in predication file can be: FAVOR, AGAINST, NONE, or UNKNOWN (case sensitive). \n";
+		print STDERR "Correct labels in predication file can be: RUMOR, NON-RUMOR, or UNKNOWN (case sensitive). \n";
 		die "\n";
 	}
 
@@ -137,7 +137,7 @@ print STDOUT sprintf("Results				 \n");
 print STDOUT sprintf("============\n");
 my $nCat = 0;
 foreach my $cat (@cats) {
-	if($cat eq "FAVOR" || $cat eq "AGAINST"){
+	if($cat eq "RUMOR" || $cat eq "NON-RUMOR"){
 		$nCat += 1;
 		$macroF += $fByCat{$cat};
 		print STDOUT sprintf("%-9s precision: %.4f recall: %.4f f-score: %.4f\n", $cat, $precByCat{$cat}, $recallByCat{$cat}, $fByCat{$cat});
@@ -154,7 +154,7 @@ sub printUsage {
 	print STDERR "Usage:\nperl eval.pl goldFile guessFile\n\n";
 	print STDERR "goldFile:  file containing gold standards;\nguessFile: file containing your prediction.\n\n";
 	print STDERR "These two files have the same format:\n";
-	print STDERR "ID<Tab>Target<Tab>Tweet<Tab>Stance\n";
+	print STDERR "ID<Tab>Target<Tab>Tweet<Tab>Veracity\n";
 	print STDERR "Only stance labels may be different between them!\n";
 	print STDERR "---------------------------\n";
 }
